eee94a0bd1e23bc56ac733a4bde30a66406daa13
Markus Schöberl <schoeby1988@gmail.com>
2025-11-09 16:25:09 +0100

    Added Bottles

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -16,7 +16,11 @@ class MovableObject extends DrawableObject {
   }
 
   isAboveGround() {
-    return this.y < 210;
+    if (this instanceof ThrowableObject) {
+      return true;
+    } else {
+      return this.y < 210;
+    }
   }
 
   // character.isColliding(chicken);
