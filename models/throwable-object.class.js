fecbd3d8581062e33655bb98f6cae4743ac06608
Markus Schöberl <schoeby1988@gmail.com>
2025-11-11 23:06:58 +0100

    Added Bottle Rotation and Splash Images

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,6 +1,24 @@
 class ThrowableObject extends MovableObject {
+  IMAGES_ROTATION = [
+    "assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png",
+    "assets/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png",
+    "assets/img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png",
+    "assets/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png",
+  ];
+
+  IMAGES_SPLASH = [
+    "assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+    "assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
+    "assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
+  ];
+
   constructor(x, y) {
     super().loadImage("assets/img/6_salsa_bottle/salsa_bottle.png");
+    this.loadImages(this.IMAGES_ROTATION);
+    this.loadImages(this.IMAGES_SPLASH);
     this.x = x;
     this.y = y;
     this.height = 60;
