25e4da5f460643f9033b89f57e44c962f77915fe
Markus Schöberl <schoeby1988@gmail.com>
2025-11-11 20:49:47 +0100

    Added Idle and Sleep Animation

--- models/character.class.js
+++ models/character.class.js
@@ -3,6 +3,10 @@ class Character extends MovableObject {
   height = 300;
   y = 80;
   speed = 10;
+  idleTime = 0;
+  idleLimit = 10000;
+  lastPositionX = this.x;
+
   IMAGES_WALKING = [
     "assets/img/2_character_pepe/2_walk/W-21.png",
     "assets/img/2_character_pepe/2_walk/W-22.png",
@@ -22,6 +26,32 @@ class Character extends MovableObject {
     "assets/img/2_character_pepe/3_jump/J-37.png",
     "assets/img/2_character_pepe/3_jump/J-38.png",
     "assets/img/2_character_pepe/3_jump/J-39.png",
+  ];
+
+  IMAGES_IDLE = [
+    "assets/img/2_character_pepe/1_idle/idle/I-1.png",
+    "assets/img/2_character_pepe/1_idle/idle/I-2.png",
+    "assets/img/2_character_pepe/1_idle/idle/I-3.png",
+    "assets/img/2_character_pepe/1_idle/idle/I-4.png",
+    "assets/img/2_character_pepe/1_idle/idle/I-5.png",
+    "assets/img/2_character_pepe/1_idle/idle/I-6.png",
+    "assets/img/2_character_pepe/1_idle/idle/I-7.png",
+    "assets/img/2_character_pepe/1_idle/idle/I-8.png",
+    "assets/img/2_character_pepe/1_idle/idle/I-9.png",
+    "assets/img/2_character_pepe/1_idle/idle/I-10.png",
+  ];
+
+  IMAGES_SLEEP = [
+    "assets/img/2_character_pepe/1_idle/long_idle/I-11.png",
+    "assets/img/2_character_pepe/1_idle/long_idle/I-12.png",
+    "assets/img/2_character_pepe/1_idle/long_idle/I-13.png",
+    "assets/img/2_character_pepe/1_idle/long_idle/I-14.png",
+    "assets/img/2_character_pepe/1_idle/long_idle/I-15.png",
+    "assets/img/2_character_pepe/1_idle/long_idle/I-16.png",
+    "assets/img/2_character_pepe/1_idle/long_idle/I-17.png",
+    "assets/img/2_character_pepe/1_idle/long_idle/I-18.png",
+    "assets/img/2_character_pepe/1_idle/long_idle/I-19.png",
+    "assets/img/2_character_pepe/1_idle/long_idle/I-20.png",
   ];
 
   IMAGES_DEAD = [
@@ -45,9 +75,11 @@ class Character extends MovableObject {
 
   constructor() {
     super();
-    this.loadImage("assets/img/2_character_pepe/2_walk/W-21.png");
+    this.loadImage("assets/img/2_character_pepe/1_idle/idle/I-1.png ");
     this.loadImages(this.IMAGES_WALKING);
     this.loadImages(this.IMAGES_JUMPING);
+    this.loadImages(this.IMAGES_IDLE);
+    this.loadImages(this.IMAGES_SLEEP);
     this.loadImages(this.IMAGES_DEAD);
     this.loadImages(this.IMAGES_HURT);
     this.applyGravity();
@@ -73,18 +105,29 @@ class Character extends MovableObject {
     }, 1000 / 60);
 
     setInterval(() => {
+      if (this.x !== this.lastPositionX) {
+        this.idleTime = 0;
+      } else {
+        this.idleTime += 50;
+      }
+      this.lastPositionX = this.x;
+
       if (this.isDead()) {
         this.playAnimation(this.IMAGES_DEAD);
       } else if (this.isHurt()) {
         this.playAnimation(this.IMAGES_HURT);
       } else if (this.isAboveGround()) {
         this.playAnimation(this.IMAGES_JUMPING);
+      } else if (this.idleTime >= this.idleLimit) {
+        this.playAnimation(this.IMAGES_SLEEP);
       } else {
         if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
           this.playAnimation(this.IMAGES_WALKING);
+        } else {
+          this.playAnimation(this.IMAGES_IDLE);
         }
       }
-    }, 50);
+    }, 100);
   }
 
   jump() {
